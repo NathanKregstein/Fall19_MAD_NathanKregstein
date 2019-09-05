98ecd0b55caf2b0d0302c7799dcb756a3ff34db4
Nathan Kregstein <nathankregstein@gmail.com>
2019-09-05 11:32:03 -0600

    Added Labs1
    place holder

---
+++ Labs/Lab1/MobileAppClass2/MobileAppClass2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobileAppClass2
+//
+//  Created by Nathan Kregstein on 8/29/19.
+//  Copyright © 2019 Nathan Kregstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
